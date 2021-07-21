cc31d2f9178eb0a8f85c5fc52a978102e6a276a9
insanniity <32269793+insanniity@users.noreply.github.com>
2021-07-21 18:57:54 -0300

    Colision

--- script.js
+++ script.js
@@ -47,6 +47,13 @@ const iniciarJogo = () => {
     if (snake[0].y > 15*box && direction === "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction === "up") snake[0].y = 16*box;
 
+    for (i=1; i< snake.length; i++){
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+            clearInterval(jogo);
+            alert('Game over :(')
+        }
+    }
+
     criarBG();
     criaCobrinha();
     drawFood();
@@ -65,9 +72,6 @@ const iniciarJogo = () => {
         food.x = Math.floor(Math.random()*15+1)*box;
         food.y = Math.floor(Math.random()*15+1)*box;
     }
-
-
-
 
     let newHead = {
         x: snakeX,
